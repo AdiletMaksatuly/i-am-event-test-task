d3ef9e842e6740b856b6d51ad1b120248ab32d73
AdiletMaksatuly <bakeryamaker1@gmail.com>
2023-06-08 16:23:58 +0600

    feat: implement tooltip to show layovers

--- src/components/FlightsItem/FlightsItem.tsx
+++ src/components/FlightsItem/FlightsItem.tsx
@@ -1,11 +1,29 @@
 import {Flight} from "../../models/flight.interface.ts";
 import styles from "./FlightsItem.module.css";
+import {Tooltip} from "react-tooltip";
+import ReactDOMServer from 'react-dom/server';
 
 interface FlightsItemProps {
     flight: Flight;
 }
 
 function FlightsItem({ flight }: FlightsItemProps) {
+    const TooltipContent = ReactDOMServer.renderToStaticMarkup((
+        <div>
+            <strong>Пересадки:</strong>
+            <ul>
+                {
+                    flight.layovers.map((layover) => (
+                        <li key={layover.airport}>
+                            <strong>{ layover.airport }</strong>
+                            <span>{ layover.duration }</span>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    ));
+
     return (
         <li className={styles.flight}>
             <div className={styles.cities}>
@@ -15,11 +33,20 @@ function FlightsItem({ flight }: FlightsItemProps) {
             </div>
             <div className={styles['flight-time']}>
                 <span className={styles.total}>{ flight.totalFlightTime } в пути</span>
-                <span className={styles.stops}>
-                    <mark className={styles.highlight}>Пересадки: { flight.stops }</mark>
+                <span
+                    data-tooltip-html={TooltipContent}
+                    data-tooltip-id="stops">
+                    {
+                        flight.stops &&
+                            <mark
+                                className={styles.highlight}>
+                                    Пересадки: { flight.stops }
+                            </mark>
+                    }
                 </span>
             </div>
             <strong className={styles.price}>Цена билета: { flight.price }</strong>
+            <Tooltip id="stops" />
         </li>
     );
 }
